f1d0120d59801048c6f104dcad6894895c362d61
Emil Hagbarth <emil@greenticket.dk>
2013-09-06 17:56:34 +0200

    til søren

--- src/wifipositioning/FingerPrintingNN.java
+++ src/wifipositioning/FingerPrintingNN.java
@@ -18,7 +18,49 @@ public class FingerPrintingNN {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String offlinePath = "data/MU.1.5meters.offline.trace", onlinePath = "data/MU.1.5meters.online.trace";
+		double[][] positions = {{13.0, 13.0, 0.0, 0.0}, {14.0, 14.0, 0.0, 0.0}}; 
 		
+		File offlineFile = new File(offlinePath);
+		Parser offlineParser = new Parser(offlineFile);
+		File onlineFile = new File(onlinePath);
+		Parser onlineParser = new Parser(onlineFile);
+		
+		
+		//Construct trace generator
+		for(double[] position: positions){
+			System.out.println("" + position[0]);
+		}
+		
+		TraceGenerator tg;
+		try {
+			int offlineSize = 25;
+			int onlineSize = 5;
+			tg = new TraceGenerator(offlineParser, onlineParser,offlineSize,onlineSize);
+			
+			//Generate traces from parsed files
+			tg.generate();
+			
+			//Iterate the trace generated from the offline file
+			List<TraceEntry> offlineTrace = tg.getOffline();			
+			for(TraceEntry entry: offlineTrace) {
+				//Print out coordinates for the collection point and the number of signal strength samples
+				System.out.println(entry.getGeoPosition().toString() + " - " + entry.getSignalStrengthSamples().size());				
+			}
+			
+			//Iterate the trace generated from the online file
+			List<TraceEntry> onlineTrace = tg.getOnline();			
+			for(TraceEntry entry: onlineTrace) {
+				//Print out coordinates for the collection point and the number of signal strength samples
+				System.out.println(entry.getGeoPosition().toString() + " - " + entry.getSignalStrengthSamples().size());
+			}
+			
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		/*
 		try {
 			 
 			String content = "This is the content to write into file";
@@ -29,7 +71,7 @@ public class FingerPrintingNN {
 		        throw new IOException("Unable to create " + dir.getAbsolutePath());
 		    }
  
-			// if file doesnt exists, then create it
+			// if file doesn't exists, then create it
 			if (!file.exists()) {
 				file.createNewFile();
 			}
@@ -44,6 +86,6 @@ public class FingerPrintingNN {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		*/
 	}
-
 }
