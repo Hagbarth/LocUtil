2ccfcdf839f0dfd1bb494f388277e7bac6669486
Emil Hagbarth <emil@greenticket.dk>
2013-09-06 14:20:19 +0200

    Made FingerPrintingNN class in a new package

---
+++ src/wifipositioning/FingerPrintingNN.java
@@ -0,0 +1,13 @@
+package wifipositioning;
+
+public class FingerPrintingNN {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
