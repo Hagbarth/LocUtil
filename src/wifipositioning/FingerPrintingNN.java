33efed8d8f8eb55541630659128adcfb78ee24e9
Emil Hagbarth <emil@greenticket.dk>
2013-09-06 14:37:11 +0200

    Tested creating a file

--- src/wifipositioning/FingerPrintingNN.java
+++ src/wifipositioning/FingerPrintingNN.java
@@ -1,5 +1,15 @@
 package wifipositioning;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.List;
+
+import org.pi4.locutil.io.TraceGenerator;
+import org.pi4.locutil.trace.Parser;
+import org.pi4.locutil.trace.TraceEntry;
+
 public class FingerPrintingNN {
 
 	/**
@@ -7,7 +17,33 @@ public class FingerPrintingNN {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String offlinePath = "data/MU.1.5meters.offline.trace", onlinePath = "data/MU.1.5meters.online.trace";
+		
+		try {
+			 
+			String content = "This is the content to write into file";
+ 
+			File file = new File("output/filename.txt");
+			File dir = new File("output");
+		    if (!dir.exists() && !dir.mkdirs()) {
+		        throw new IOException("Unable to create " + dir.getAbsolutePath());
+		    }
+ 
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+ 
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(content);
+			bw.close();
+ 
+			System.out.println("Done");
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
