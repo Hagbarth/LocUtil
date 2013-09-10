b29080d4bed9f2edb5d493bf9072efc4877147c1
Emil Hagbarth <emil@greenticket.dk>
2013-09-10 11:50:57 +0200

    dasdsa

--- src/wifipositioning/FingerPrintingKNN.java
+++ src/wifipositioning/FingerPrintingKNN.java
@@ -1,6 +1,6 @@
 /**
 *
-* Implementation of the K Nearest Neighbors Algorithm
+* Implementatidasason of the K Nearest Neighbors Algorithm
 *
 * @author Fluo (Søren Lundtof, Laurits Langberg and Emil Rasmussen)
 * @version 1.0.1
