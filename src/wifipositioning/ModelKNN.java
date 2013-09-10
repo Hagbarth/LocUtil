377f68a77d1e169ffa78fc6e695df380a61f43f6
Emil Hagbarth <emil@greenticket.dk>
2013-09-10 12:01:22 +0200

    Made Class stub for ModeKNN

---
+++ src/wifipositioning/ModelKNN.java
@@ -0,0 +1,20 @@
+class ModelKNN{
+	private int k;
+
+	public static void main(String[] args) {
+		int k = args[0];
+		ModelKNN knn = new ModelKNN(k);
+	}
+
+	public ModelKNN(int k){
+		this.k = k;
+	}
+
+	public String doKNN(){
+		//TODO implement modelKNN algorithm and return results in a string
+	}
+
+	public void printKnnToFile(){
+		//Todo print results fomr doKNN to a file in ../output
+	}
+}
